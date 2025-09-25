83432531e367700935e79a28148d04c19586732e
Ilya Kolomoytsev <i.kolomoitsev@yandex.ru>
2025-09-25 23:05:37 +0300

    feat(lexer): input parameter for the main function is the file to be analyzed

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <fstream>
 #include <FlexLexer.h>
 
-int main() {
-    std::cout << "Введите текст для анализа: \n";
+int main(int argc, char* argv[]) {
+    // Проверка на наличие аргумента командной строки (пути к файлу)
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <file_path>" << std::endl;
+        return 1;
+    }
+
+    // Открываем файл по пути, переданному в аргументе
+    std::ifstream file(argv[1]);
+    if (!file.is_open()) {
+        std::cerr << "Error opening file: " << argv[1] << std::endl;
+        return 1;
+    }
 
     yyFlexLexer lexer;       // создаём объект лексера
+    lexer.switch_streams(&file, &std::cout);  // Передаем файл для анализа
     lexer.yylex();           // вызываем метод yylex() класса
 
     return 0;
