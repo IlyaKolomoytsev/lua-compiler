7d0c9885b6e8d0356cbb6ce7b7c6f2eed98fbe79
Ilya Kolomoytsev <i.kolomoitsev@yandex.ru>
2025-10-27 00:12:39 +0300

    feat(parser): introduce statement node class

--- nodes/StatementNode.cpp
+++ nodes/StatementNode.cpp
@@ -1,1 +1,250 @@
 #include "StatementNode.h"
+
+#include <assert.h>
+
+StatementNode* StatementNode::Declaration(NameList* names)
+{
+    StatementNode* node = new StatementNode(Type::Declaration);
+    node->value_.declaration_v = names;
+    return node;
+}
+
+StatementNode* StatementNode::Assignment(Scope scope, NameList* names, ExpressionNodeList* values)
+{
+    StatementNode* node = new StatementNode(Type::Assignment);
+    assigment_t* assignment = &node->value_.assigment_v;
+    assignment->scope = scope;
+    assignment->names = names;
+    assignment->values = values;
+    return node;
+}
+
+StatementNode* StatementNode::FunctionCall(ExpressionNode* functionId, ExpressionNodeList* arguments)
+{
+    StatementNode* node = new StatementNode(Type::FunctionCall);
+    node->value_.functionCall_v = ExpressionNode::FunctionCall(functionId, arguments);
+    return node;
+}
+
+StatementNode* StatementNode::FunctionCall(ExpressionNode* functionId, ExpressionNode* argument)
+{
+    ExpressionNodeList* arguments = new ExpressionNodeList();
+    arguments->push_back(argument);
+
+    StatementNode* node = FunctionCall(functionId, arguments);
+    return node;
+}
+
+StatementNode* StatementNode::Branching(
+    ExpressionNode* condition,
+    StatementNode* successBlock,
+    StatementNode* failureBlock
+)
+{
+    StatementNode* node = new StatementNode(Type::Branching);
+    branching_t* branching = &node->value_.branching_v;
+    branching->condition = condition;
+    branching->successBlock = successBlock;
+    branching->failureBlock = failureBlock;
+    return node;
+}
+
+StatementNode* StatementNode::ForLoop(ExpressionNode* id, ForRangeStruct range, StatementNode* block)
+{
+    StatementNode* node = new StatementNode(Type::ForLoop);
+    for_loop_t* loop = &node->value_.forLoop_v;
+    loop->iteratorVariable = id;
+    loop->range = range;
+    loop->block = block;
+    return node;
+}
+
+StatementNode* StatementNode::ForLoop(NameList* names, ExpressionNodeList* explist, StatementNode* block)
+{
+    auto converter = ForLoopIteratorConverter(names, explist, block);
+    return converter.generate();
+}
+
+StatementNode* StatementNode::WhileLoop(ExpressionNode* condition, StatementNode* block)
+{
+    StatementNode* node = new StatementNode(Type::WhileLoop);
+    while_loop_t* loop = &node->value_.whileLoop_v;
+    loop->condition = condition;
+    loop->block = block;
+    return node;
+}
+
+StatementNode* StatementNode::RepeatLoop(StatementNode* block, ExpressionNode* condition)
+{
+    StatementNode* node = new StatementNode(Type::RepeatLoop);
+    repeat_loop_t* loop = &node->value_.repeatLoop_v;
+    loop->condition = condition;
+    loop->block = block;
+    return node;
+}
+
+StatementNode* StatementNode::Block(StatementNodeList* statements, StatementNode* finishStatement)
+{
+    StatementNode* node = new StatementNode(Type::Block);
+    if (finishStatement != nullptr)
+    {
+        statements->push_back(node);
+    }
+    node->value_.block_v = statements;
+    return node;
+}
+
+StatementNode* StatementNode::GoTo(label_t label)
+{
+    StatementNode* node = new StatementNode(Type::GoTo);
+    node->value_.goTo_v = label;
+    return node;
+}
+
+StatementNode* StatementNode::Label(label_t label)
+{
+    StatementNode* node = new StatementNode(Type::Label);
+    node->value_.label_v = label;
+    return node;
+}
+
+StatementNode* StatementNode::Break()
+{
+    StatementNode* node = new StatementNode(Type::Break);
+    return node;
+}
+
+StatementNode* StatementNode::Return(ExpressionNodeList* values)
+{
+    StatementNode* node = new StatementNode(Type::Return);
+    node->value_.return_v = values;
+    return node;
+}
+
+inline StatementNode::Type StatementNode::getType()
+{
+    return type_;
+}
+
+StatementNode::StatementNode(Type type) : type_(type)
+{
+}
+
+StatementNode::ForLoopIteratorConverter::ForLoopIteratorConverter(
+    NameList* names, ExpressionNodeList* explist, StatementNode* block
+) : names_(names), explist_(explist), block_(block)
+{
+    // assert for name list
+    bool correctNameListSize = names->size() > 0;
+    assert(correctNameListSize);
+
+    // assert for expression list
+    bool correctExplistSize = explist->size() > 0;
+    assert(correctExplistSize);
+
+    // assert for block
+    StatementNode::Type blockType = block->getType();
+    assert(blockType == StatementNode::Type::Block);
+}
+
+StatementNode* StatementNode::ForLoopIteratorConverter::generate()
+{
+    StatementNodeList* statements = new StatementNodeList();
+    statements->push_back(getExplistAssigment());
+    statements->push_back(getWhileLoop());
+    StatementNode* result = StatementNode::Block(statements);
+    return result;
+}
+
+inline StatementNode* StatementNode::ForLoopIteratorConverter::getExplistAssigment()
+{
+    return StatementNode::Assignment(
+        Scope::Local,
+        new NameList{
+            getF(),
+            getS(),
+            getVar(),
+        },
+        explist_
+    );
+}
+
+StatementNode* StatementNode::ForLoopIteratorConverter::getWhileLoop()
+{
+    StatementNodeList* statements = new StatementNodeList();
+    statements->push_back(getInteratorResultAssigment());
+    statements->push_back(getExitBranching());
+    statements->push_back(getVarAssigment());
+    statements->push_back(block_);
+    return StatementNode::WhileLoop(
+        ExpressionNode::Bool(true),
+        StatementNode::Block(statements)
+    );
+}
+
+inline StatementNode* StatementNode::ForLoopIteratorConverter::getInteratorResultAssigment()
+{
+    ExpressionNode* fCall = ExpressionNode::FunctionCall(
+        getF(),
+        new ExpressionNodeList{
+            getS(),
+            getVar(),
+        }
+    );
+
+    StatementNode* assignment = StatementNode::Assignment(
+        Scope::Local,
+        names_,
+        new ExpressionNodeList{fCall}
+    );
+
+    return assignment;
+}
+
+inline StatementNode* StatementNode::ForLoopIteratorConverter::getExitBranching()
+{
+    ExpressionNode* condition = ExpressionNode::Equality(
+        getFirstName(),
+        ExpressionNode::Nil()
+    );
+    StatementNode* successBlock = StatementNode::Block(
+        new StatementNodeList{
+            StatementNode::Break()
+        }
+    );
+    return StatementNode::Branching(
+        condition,
+        successBlock
+    );
+}
+
+inline StatementNode* StatementNode::ForLoopIteratorConverter::getVarAssigment()
+{
+    return StatementNode::Assignment(
+        Scope::Global,
+        new NameList{getVar()},
+        new ExpressionNodeList{getFirstName()}
+    );
+}
+
+ExpressionNode* StatementNode::ForLoopIteratorConverter::getF()
+{
+    return ExpressionNode::Id(new std::string("f"));
+}
+
+ExpressionNode* StatementNode::ForLoopIteratorConverter::getS()
+{
+    return ExpressionNode::Id(new std::string("s"));
+}
+
+ExpressionNode* StatementNode::ForLoopIteratorConverter::getVar()
+{
+    return ExpressionNode::Id(new std::string("var"));
+}
+
+ExpressionNode* StatementNode::ForLoopIteratorConverter::getFirstName()
+{
+    std::string* firstName = names_->front()->getId();
+    std::string* nameCopy = new std::string(*firstName);
+    return ExpressionNode::Id(nameCopy);
+}
--- nodes/StatementNode.h
+++ nodes/StatementNode.h
@@ -1,9 +1,254 @@
 #ifndef LUA_COMPILER_STATEMENT_NODE_H
 #define LUA_COMPILER_STATEMENT_NODE_H
+#include <functional>
+#include <vector>
 
+#include "ExpressionNode.h"
+
+class StatementNode;
+
+using StatementNodeList = std::list<StatementNode*>;
+
+struct ForRangeStruct
+{
+    ExpressionNode* start;
+    ExpressionNode* finish;
+    ExpressionNode* step;
+};
+
+enum class Scope
+{
+    Global,
+    Local,
+};
 
 class StatementNode
 {
+    using declaration_type_t = NameList*;
+    using label_t = uint;
+
+    struct assigment_t
+    {
+        Scope scope;
+        NameList* names;
+        ExpressionNodeList* values;
+    };
+
+    using function_call_t = ExpressionNode*;
+
+    struct branching_t
+    {
+        ExpressionNode* condition;
+        StatementNode* successBlock;
+        StatementNode* failureBlock;
+    };
+
+    struct for_loop_t
+    {
+        ExpressionNode* iteratorVariable;
+        ForRangeStruct range;
+        StatementNode* block;
+    };
+
+    struct simple_loop
+    {
+        ExpressionNode* condition;
+        StatementNode* block;
+    };
+
+    using while_loop_t = simple_loop;
+    using repeat_loop_t = simple_loop;
+    using block_t = StatementNodeList*;
+    using return_t = ExpressionNodeList*;
+
+    union Value
+    {
+        declaration_type_t declaration_v;
+        assigment_t assigment_v;
+        function_call_t functionCall_v;
+        branching_t branching_v;
+        for_loop_t forLoop_v;
+        while_loop_t whileLoop_v;
+        repeat_loop_t repeatLoop_v;
+        block_t block_v;
+        label_t goTo_v;
+        label_t label_v;
+        return_t return_v;
+    };
+
+public:
+    enum class Type
+    {
+        Declaration,
+        Assignment,
+        FunctionCall,
+        Branching,
+        ForLoop,
+        WhileLoop,
+        RepeatLoop,
+        Block,
+        GoTo,
+        Label,
+        Break,
+        Return,
+    };
+
+    static StatementNode* Declaration(NameList* names);
+
+    static StatementNode* Assignment(Scope scope, NameList* names, ExpressionNodeList* values);
+
+    static StatementNode* FunctionCall(ExpressionNode* functionId, ExpressionNodeList* arguments);
+
+    static StatementNode* FunctionCall(ExpressionNode* functionId, ExpressionNode* argument);
+
+    static StatementNode* Branching(
+        ExpressionNode* condition,
+        StatementNode* successBlock,
+        StatementNode* failureBlock = nullptr
+    );
+
+    static StatementNode* ForLoop(ExpressionNode* id, ForRangeStruct range, StatementNode* block);
+
+    static StatementNode* ForLoop(NameList* names, ExpressionNodeList* iterator, StatementNode* block);
+
+    static StatementNode* WhileLoop(ExpressionNode* condition, StatementNode* block);
+
+    static StatementNode* RepeatLoop(StatementNode* block, ExpressionNode* condition);
+
+    static StatementNode* Block(StatementNodeList* statements, StatementNode* finishStatement = nullptr);
+
+    static StatementNode* GoTo(label_t label);
+
+    static StatementNode* Label(label_t label);
+
+    static StatementNode* Break();
+
+    static StatementNode* Return(ExpressionNodeList* values);
+
+    Type getType();
+
+private:
+    explicit StatementNode(Type type);
+
+    Type type_;
+    Value value_;
+
+
+    /**
+     * Class interpret this statement
+     * @code
+     * for var_1, ···, var_n in explist do block end
+     * @endcode
+     * to this statement
+     * @code
+     * do
+     *     local f, s, var = explist
+     *     while true do
+     *         local var_1, ···, var_n = f(s, var)
+     *         if var_1 == nil then break end
+     *         var = var_1
+     *         block
+     *     end
+     * end
+     * @endcode
+     */
+    class ForLoopIteratorConverter
+    {
+    public:
+        /**
+         * Create converter class and validate input parameters.
+         * @param names name list of: "var_1, ···, var_n".
+         * @param explist expression list.
+         * @param block for body block statement.
+         */
+        ForLoopIteratorConverter(NameList* names, ExpressionNodeList* explist, StatementNode* block);
+
+        /**
+         * Generate converted for loop statement node.
+         * @return Converted for loop.
+         */
+        StatementNode* generate();
+
+    private:
+        /**
+         * Generate expression list assigment statement node:
+         * @code
+         * local f, s, var = explist
+         * @endcode
+         *
+         * @return Expression list assigment.
+         */
+        StatementNode* getExplistAssigment();
+
+        /**
+         * Generate while loop statement node:
+         * @code
+         * while true do
+         *     local var_1, ···, var_n = f(s, var)
+         *     if var_1 == nil then break end
+         *     var = var_1
+         *     block
+         * end
+         * @endcode
+         *
+         * @return While loop.
+         */
+        StatementNode* getWhileLoop();
+
+        /**
+         * Generate iterator function result assigment statement node:
+         * @code
+         * local var_1, ···, var_n = f(s, var)
+         * @endcode
+         *
+         * @return Iterator result assigment.
+         */
+        StatementNode* getInteratorResultAssigment();
+
+        /**
+         * Generate exit branching statement node:
+         * @code
+         * if var_1 == nil then break end
+         * @endcode
+         *
+         * @return Exit branching.
+         */
+        StatementNode* getExitBranching();
+
+        /**
+         * Generate var assigment statement node:
+         * @code
+         * var = var_1
+         * @endcode
+         *
+         * @return
+         */
+        StatementNode* getVarAssigment();
+
+        /**
+         * @brief Generate f variable expression node.
+         * @return F variable.
+         */
+        ExpressionNode* getF();
+
+        /**
+         * @brief Generate s variable expression node.
+         * @return S variable.
+         */
+        ExpressionNode* getS();
+
+        /**
+         * @brief Generate var variable expression node.
+         * @return Var variable.
+         */
+        ExpressionNode* getVar();
+
+        ExpressionNode* getFirstName();
+
+        NameList* names_;
+        ExpressionNodeList* explist_;
+        StatementNode* block_;
+    };
 };
 
 
