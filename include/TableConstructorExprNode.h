4964ce9abfb91b0d6dfcb856f4028ada7e7b5e7f
Ilya Kolomoytsev <i.kolomoitsev@yandex.ru>
2026-01-16 19:50:56 +0300

    feat: introduce table constructor expression node

---
+++ include/TableConstructorExprNode.h
@@ -0,0 +1,18 @@
+#ifndef LUA_COMPILER_TABLE_CONSTRUCTOR_EXPR_NODE_H
+#define LUA_COMPILER_TABLE_CONSTRUCTOR_EXPR_NODE_H
+#include "ExpressionNode.h"
+
+class TableConstructorExprNode : public ExpressionNode
+{
+public:
+    TableConstructorExprNode(const TableFieldList& fields) : ExpressionNode(Type::TableConstructor), fields_(fields)
+    {
+    }
+
+    [[nodiscard]] const TableFieldList& tableFields() const;
+
+private:
+    TableFieldList fields_;
+};
+
+#endif //LUA_COMPILER_TABLE_CONSTRUCTOR_EXPR_NODE_H
