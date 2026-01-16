758a2d9944d1d8016c70b4e030aece19ea7ecbb7
Ilya Kolomoytsev <i.kolomoitsev@yandex.ru>
2026-01-16 20:30:06 +0300

    feat: introduce function call expression node

---
+++ include/FunctionCallExprNode.h
@@ -0,0 +1,20 @@
+#ifndef LUA_COMPILER_FUNCTION_CALL_EXPR_NODE_H
+#define LUA_COMPILER_FUNCTION_CALL_EXPR_NODE_H
+#include "ExpressionNode.h"
+
+class FunctionCallExprNode : public ExpressionNode
+{
+public:
+    FunctionCallExprNode(ExpressionNode* functionExpr, const FunctionArgumentsList& arguments) :
+    ExpressionNode(Type::FunctionCall), functionExpr_(functionExpr), arguments_(arguments)
+    {
+    }
+
+    [[nodiscard]] ExpressionNode* getFunctionExpression() const;
+    [[nodiscard]] const ExpressionNodeList& getFunctionArguments() const;
+private:
+    ExpressionNode* functionExpr_;
+    FunctionArgumentsList arguments_;
+};
+
+#endif //LUA_COMPILER_FUNCTION_CALL_EXPR_NODE_H
