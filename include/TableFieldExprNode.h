b93435e4402095c48e7c4f45f5c8f0fbad71be1d
Ilya Kolomoytsev <i.kolomoitsev@yandex.ru>
2026-01-16 19:45:57 +0300

    feat: introduce table field expression node

---
+++ include/TableFieldExprNode.h
@@ -0,0 +1,21 @@
+#ifndef LUA_COMPILER_TABLE_FIELD_EXPR_NODE_H
+#define LUA_COMPILER_TABLE_FIELD_EXPR_NODE_H
+#include "ExpressionNode.h"
+
+class TableFieldExprNode : public ExpressionNode
+{
+public:
+    explicit TableFieldExprNode(ExpressionNode* table, ExpressionNode* key)
+        : ExpressionNode(Type::TableField), table_(table), key_(key)
+    {
+    }
+
+    [[nodiscard]] ExpressionNode* getTable() const { return table_; }
+    [[nodiscard]] ExpressionNode* getKey() const { return key_; }
+
+private:
+    ExpressionNode* table_;
+    ExpressionNode* key_;
+};
+
+#endif //LUA_COMPILER_TABLE_FIELD_EXPR_NODE_H
