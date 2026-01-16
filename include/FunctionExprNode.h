5c45a0048edbf0fb3ed7188f86eb475e70a7c91d
Ilya Kolomoytsev <i.kolomoitsev@yandex.ru>
2026-01-16 20:44:14 +0300

    feat: introduce function expression node

---
+++ include/FunctionExprNode.h
@@ -0,0 +1,21 @@
+#ifndef LUA_COMPILER_FUNCTION_EXPR_NODE_H
+#define LUA_COMPILER_FUNCTION_EXPR_NODE_H
+#include "ExpressionNode.h"
+
+class FunctionExprNode : public ExpressionNode
+{
+public:
+    FunctionExprNode(NameList* parameters, ExpressionNode* body) :
+        ExpressionNode(Type::FunctionLiteral), parameters_(parameters), body_(body)
+    {
+    }
+
+    [[nodiscard]] const NameList& parameters() const { return *parameters_; };
+    [[nodiscard]] ExpressionNode* body() const { return body_; }
+
+private:
+    NameList* parameters_;
+    ExpressionNode* body_;
+};
+
+#endif //LUA_COMPILER_FUNCTION_EXPR_NODE_H
