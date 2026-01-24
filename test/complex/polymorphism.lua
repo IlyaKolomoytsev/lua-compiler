ad82e0f40bb51932696e05a5cf420d2c6f8bce87
KarbonST <mikhailbykadorov@mail.ru>
2026-01-25 02:00:52 +0300

    fix(test): use polymorphism with using metatables

--- test/complex/polymorphism.lua
+++ test/complex/polymorphism.lua
@@ -1,21 +1,46 @@
-function area(shape)
-    return shape:area()
+Shape = {}
+Shape.__index = Shape
+
+function Shape:area()
+    return 0
 end
 
-circle = {
-    r = 10,
-    area = function(self)
-        return 3.14 * self.r * self.r
-    end
-}
+Circle = {}
+Circle.__index = Circle
+setmetatable(Circle, Shape)
 
-rect = {
-    w = 4,
-    h = 5,
-    area = function(self)
-        return self.w * self.h
-    end
-}
+function Circle:new(r)
+    return setmetatable({ r = r }, self)
+end
 
-print(area(circle)) -- 314.15
-print(area(rect))   -- 20
+function Circle:area()
+    return 3.14 * self.r * self.r
+end
+
+Rectangle = {}
+Rectangle.__index = Rectangle
+setmetatable(Rectangle, Shape)
+
+function Rectangle:new(w, h)
+    return setmetatable({ w = w, h = h }, self)
+end
+
+function Rectangle:area()
+    return self.w * self.h
+end
+
+Square = {}
+Square.__index = Square
+setmetatable(Square, Rectangle)
+
+function Square:new(s)
+    return setmetatable(Rectangle:new(s, s), self)
+end
+
+c = Circle:new(10)
+r = Rectangle:new(4, 5)
+s = Square:new(5)
+
+print(c:area())  -- 314
+print(r:area())  -- 20
+print(s:area()) -- 25
