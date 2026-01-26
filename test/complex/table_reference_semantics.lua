c2d34f7183f543e3fc4cd280d86b4f25b10d2725
KarbonST <mikhailbykadorov@mail.ru>
2026-01-26 14:51:26 +0300

    test: new test for table reference semantics

--- test/complex/table_reference_semantics.lua
+++ test/complex/table_reference_semantics.lua
@@ -1,11 +1,23 @@
-table = {};
-f = function (p) -- Передаем копию ссылки на объект
-    p.field = 1 -- Меняем внутреннее состояние объекта
-    print("param =", p)
-    p = 2; -- Внешний объект не перезаписывается
-    print("param =", p)
+obj = { name = "Lua" }
+arr = { 10, 20, 30 }
+num = 5
+str = "hello"
+
+f = function(n, s, a, o)
+    print("inside start:", n, s, a[1], o.name)
+
+    n = 100
+    s = "world"
+
+    a[1] = 999
+    o.name = "Changed"
+
+    a = { 1, 2, 3 }
+    o = { name = "NewObj" }
+
+    print("inside end:", n, s, a[1], o.name)
 end
 
-f(table)
-print("table =", table)
-print("field =", table.field)
+f(num, str, arr, obj)
+
+print("outside:", num, str, arr[1], obj.name)
